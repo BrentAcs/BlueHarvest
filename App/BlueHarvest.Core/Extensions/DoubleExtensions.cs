05faf95508f1fc073cae890d46b723e011472936
Brent Acs <brent.mann@gmai.com>
2021-11-18 06:05:29 -0500

    added extensions for ly to/from au and au to/from km.

--- App/BlueHarvest.Core/Extensions/DoubleExtensions.cs
+++ App/BlueHarvest.Core/Extensions/DoubleExtensions.cs
@@ -10,5 +10,21 @@ public static class DoubleExtensions
 
       return Math.Abs(left - right) < precision;
    }
+
+   public const double AuPerLightYear = 63241.1;
+   public const double KmPerAu = 1.496e+8;
+   
+   public static double LightYearToAu(this double ly) =>
+      ly * AuPerLightYear;
+
+   public static double AuToLightYear(this double au) =>
+      au / AuPerLightYear;
+
+   public static double AuToKm(this double au) =>
+      au * KmPerAu;
+   
+   public static double KmToAu(this double km) =>
+      km / KmPerAu;
+
 }
 
