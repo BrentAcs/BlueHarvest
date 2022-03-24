79def0a2409b5ae202f4ac3e113eed31ad67b3f1
Brent Acs <brent.mann@gmail.com>
2022-03-24 13:58:23 -0400

    work in progress on the indexed prompt.

--- BlueHarvest.Consoul.BuilderRnD/Previews/Tables/StarClusterTablePreview.cs
+++ BlueHarvest.Consoul.BuilderRnD/Previews/Tables/StarClusterTablePreview.cs
@@ -1,3 +1,4 @@
+using BlueHarvest.Consoul.BuilderRnD.Menus;
 using BlueHarvest.Consoul.Common;
 using BlueHarvest.Core.Rnd;
 
@@ -9,14 +10,66 @@ public class StarClusterTablePreview : TablePreview
 
    protected override void ShowPreview()
    {
+      var items = new List<AnsiConSoul.IndexedPromptString>();
+      items.Add((-1, "none"));
+
       FakeFactory.Shallow = true;
       var col = new List<StarCluster>();
-      for (int i = 0; i < 50; i++)
+      for (int i = 0; i < 20; i++)
       {
-         col.Add(FakeFactory.CreateStarCluster());
+         var cluster = FakeFactory.CreateStarCluster();
+         col.Add(cluster);
+         items.Add((i + 1, cluster.Name));
       }
 
       var table = col.Build();
       AnsiConsole.Write(table);
+
+      var result = AnsiConSoul.Prompt(items, "pick one, bitch", 4);
+      AnsiConsole.WriteLine($"{result}");
+   }
+}
+
+public static class AnsiConSoul
+{
+   public class IndexedPromptItem<T> where T : notnull
+   {
+      //public static readonly IndexedPromptItem<T> None = new IndexedPromptItem<T>(-1, null, "None"); 
+      
+      public int Index { get; set; }
+      public string? Text { get; set; }
+      public T? Item { get; set; }
+
+      public IndexedPromptItem(int index, T item, string? text = null)
+      {
+         Index = index;
+         Item = item;
+         Text = text ?? item.ToString();
+      }
+
+      public static IndexedPromptItem<T> Create(int index, T item, string? text = null) => new(index, item, text);
+
+      public override string? ToString() => Text;
+   }
+
+   public class IndexedPromptString : IndexedPromptItem<string>
+   {
+      public IndexedPromptString(int index, string item, string? text = null) : base(index, item, text)
+      {
+      }
+   }
+   
+
+   public static T Prompt<T>(IEnumerable<T> items, string title, int pageSize, Style? highlightStyle = null, Style? disabledStyle = null)
+   {
+      var prompt = new SelectionPrompt<T>()
+         .PageSize(pageSize)
+         .Title(title)
+         .HighlightStyle(highlightStyle)
+         .AddChoices(items);
+      prompt.DisabledStyle = disabledStyle;
+
+      T result = AnsiConsole.Prompt(prompt);
+      return result;
    }
 }
--- BlueHarvest.Consoul.BuilderRnD/Previews/Trees/StarClusterTreePreview.cs
+++ BlueHarvest.Consoul.BuilderRnD/Previews/Trees/StarClusterTreePreview.cs
@@ -1,6 +1,7 @@
 ﻿using BlueHarvest.Consoul.BuilderRnD.Previews.Trees;
 using BlueHarvest.Consoul.Common;
 using BlueHarvest.Core.Rnd;
+using BlueHarvest.Core.Rnd.Utilities;
 
 namespace BlueHarvest.Consoul.BuilderRnD.Previews.Tables;
 
@@ -19,7 +20,22 @@ public class StarClusterTreePreview : TreePreview
       //
       // var table = col.Build();
       // AnsiConsole.Write(table);
+
+      //EntityMonikerGeneratorService.Default.Generate();
+
+      var moniker = EntityMonikerGeneratorService.Default;
+
+      var root = new Tree(moniker.Generate());
+      root.AddNode(moniker.Generate());
+      var node = root.AddNode(moniker.Generate()).Collapse();
+      node.AddNode("child");
+      //.Collapse();
+      root.AddNode(moniker.Generate());
+
+      AnsiConsole.Write(root);
       
-      AnsiConsole.WriteLine("boobs");
+      AnsiConsole.WriteLine("presto!");
+      Console.ReadKey(true);
+      node.Expand();
    }
 }
