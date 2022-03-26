3162f59ec5e7aa0b14f681f341dd591be5154760
Brent Acs <brent.mann@gmail.com>
2022-03-26 17:34:27 -0400

    WiP

--- App.ConSoul/BlueHarvest.ConSoul.BuilderRnD/Program.cs
+++ App.ConSoul/BlueHarvest.ConSoul.BuilderRnD/Program.cs
@@ -1,5 +1,6 @@
 ﻿using BlueHarvest.ConSoul.BuilderRnD;
 using BlueHarvest.ConSoul.BuilderRnD.Menus;
+using BlueHarvest.ConSoul.BuilderRnD.Views;
 using BlueHarvest.Core.Rnd;
 using BlueHarvest.Core.Rnd.Extensions;
 
@@ -7,7 +8,7 @@ Console.Title = "Blue Harvest ConSoul Builder RnD";
 
 if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
 {
-   Console.WindowHeight = 50;
+   Console.WindowHeight = 40;
 }
 
 MainMenu.ShowMenu();
@@ -16,7 +17,7 @@ public class StarClusterBrowser
 {
    public static void Show()
    {
-      Console.WriteLine("Star Cluster Browser");
+      //Console.WriteLine("Star Cluster Browser");
 
 #if false
       // create new star cluster
@@ -29,28 +30,31 @@ public class StarClusterBrowser
 #else
       // load a star cluster
 
-      var favorites = AnsiConsole.Prompt(
-         new SelectionPrompt<string>()
-            .PageSize(10)
-            .Title("What are your [green]favorite fruits[/]?")
-            .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
-            .AddChoiceGroup("Berries", new[]
-            {
-               "Blackcurrant", "Blueberry", "Cloudberry",
-               "Elderberry", "Honeyberry", "Mulberry"
-            })
-            .AddChoices(new[]
-            {
-               "Apple", "Apricot", "Avocado", "Banana",
-               "Cherry", "Cocunut", "Date", "Dragonfruit", "Durian",
-               "Egg plant",  "Fig", "Grape", "Guava",
-               "Jackfruit", "Jambul", "Kiwano", "Kiwifruit", "Lime", "Lylo",
-               "Lychee", "Melon", "Nectarine", "Orange", "Olive"
-            }));      
-      
-      var cluster = "../../../../../SampleData/test-starcluster.json".FromJsonFile<StarCluster>();
+      //var cluster = "../../../../../SampleData/test-starcluster.json".FromJsonFile<StarCluster>();
 #endif
+      
+      new StarClusterView().Show();
+      
+      // var favorites = AnsiConsole.Prompt(
+      //    new SelectionPrompt<string>()
+      //       .PageSize(10)
+      //       .Title("What are your [green]favorite fruits[/]?")
+      //       //.MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+      //       .AddChoiceGroup("Berries", new[]
+      //       {
+      //          "Blackcurrant", "Blueberry", "Cloudberry",
+      //          "Elderberry", "Honeyberry", "Mulberry"
+      //       })
+      //       .AddChoices(new[]
+      //       {
+      //          "Apple", "Apricot", "Avocado", "Banana",
+      //          "Cherry", "Cocunut", "Date", "Dragonfruit", "Durian",
+      //          "Egg plant",  "Fig", "Grape", "Guava",
+      //          "Jackfruit", "Jambul", "Kiwano", "Kiwifruit", "Lime", "Lylo",
+      //          "Lychee", "Melon", "Nectarine", "Orange", "Olive"
+      //       }));      
 
+      
       // Console.WriteLine($"cluster planetary count: {cluster?.PlanetarySystems.Count()}");
       // cluster.p
       Console.WriteLine("press any key.");
---
+++ App.ConSoul/BlueHarvest.ConSoul.BuilderRnD/Views/View.cs
@@ -0,0 +1,41 @@
+﻿namespace BlueHarvest.ConSoul.BuilderRnD.Views;
+
+public abstract class View
+{
+   protected virtual string? Header => null;
+   protected virtual string? Footer => null;
+   
+   protected abstract void ShowView();
+
+   public void Show()
+   {
+      if (Header is not null)
+         AnsiConsole.WriteLine($"{Header}");
+
+      ShowView();
+
+      if (Footer is not null)
+         AnsiConsole.WriteLine($"{Footer}");
+
+      //AnsiConsole.WriteLine($"{Prompt}");
+      Console.ReadKey(true);
+   }
+}
+
+public class StarClusterView : View
+{
+   protected override void ShowView() =>
+      Console.WriteLine("Star Cluster View");
+}
+
+public class PlanetarySystemView : View
+{
+   protected override void ShowView() =>
+      Console.WriteLine("Planetary System View");
+}
+
+public class SatelliteSystemView : View
+{
+   protected override void ShowView() =>
+      Console.WriteLine("Satellite System View");
+}
