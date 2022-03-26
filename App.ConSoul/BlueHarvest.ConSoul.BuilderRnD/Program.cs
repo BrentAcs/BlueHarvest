3eef78f24806e03ae280bb58057a299abbb28887
Brent Acs <brent.mann@gmail.com>
2022-03-26 13:01:26 -0400

    WiP

--- App.ConSoul/BlueHarvest.ConSoul.BuilderRnD/Program.cs
+++ App.ConSoul/BlueHarvest.ConSoul.BuilderRnD/Program.cs
@@ -18,22 +18,42 @@ public class StarClusterBrowser
    {
       Console.WriteLine("Star Cluster Browser");
 
-#if true
+#if false
       // create new star cluster
 
       // var options = (FakeFactory.StarClusterOptions)null;
       // var options = FakeFactory.StarClusterOptions.Empty;
-      var options = new FakeFactory.StarClusterOptions {PlanetarySystemCount = 1000};
+      var options = new FakeFactory.StarClusterOptions {PlanetarySystemCount = 100};
       var cluster = FakeFactory.CreateStarCluster(options);
       cluster.ToJsonFile("../../../../../SampleData/test-starcluster.json", JsonSettings.FormattedTypedNamedEnums);
 #else
       // load a star cluster
 
+      var favorites = AnsiConsole.Prompt(
+         new SelectionPrompt<string>()
+            .PageSize(10)
+            .Title("What are your [green]favorite fruits[/]?")
+            .MoreChoicesText("[grey](Move up and down to reveal more fruits)[/]")
+            .AddChoiceGroup("Berries", new[]
+            {
+               "Blackcurrant", "Blueberry", "Cloudberry",
+               "Elderberry", "Honeyberry", "Mulberry"
+            })
+            .AddChoices(new[]
+            {
+               "Apple", "Apricot", "Avocado", "Banana",
+               "Cherry", "Cocunut", "Date", "Dragonfruit", "Durian",
+               "Egg plant",  "Fig", "Grape", "Guava",
+               "Jackfruit", "Jambul", "Kiwano", "Kiwifruit", "Lime", "Lylo",
+               "Lychee", "Melon", "Nectarine", "Orange", "Olive"
+            }));      
+      
       var cluster = "../../../../../SampleData/test-starcluster.json".FromJsonFile<StarCluster>();
 #endif
 
       // Console.WriteLine($"cluster planetary count: {cluster?.PlanetarySystems.Count()}");
       // cluster.p
+      Console.WriteLine("press any key.");
       Console.ReadKey(true);
    }
 }
